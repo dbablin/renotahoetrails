79084b6e722cd38ac95ad491d4e533dfb03b2aa5
Daniel Bablinskas <dbablin@gmail.com>
2020-07-03 06:24:40 -0700

    Updates

---
+++ precache-manifest.5c279046d20fc591ca4b536e271a82d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03344a337790e07806ef155d4f99c44d",
+    "url": "/renotahoetrails/index.html"
+  },
+  {
+    "revision": "fef124b431534972822f",
+    "url": "/renotahoetrails/static/css/2.de04c12b.chunk.css"
+  },
+  {
+    "revision": "9f996de73da06e220dd2",
+    "url": "/renotahoetrails/static/css/main.59ee7241.chunk.css"
+  },
+  {
+    "revision": "fef124b431534972822f",
+    "url": "/renotahoetrails/static/js/2.d20df7a4.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/renotahoetrails/static/js/2.d20df7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f996de73da06e220dd2",
+    "url": "/renotahoetrails/static/js/main.74538f03.chunk.js"
+  },
+  {
+    "revision": "9ca4374ed5d1441be603",
+    "url": "/renotahoetrails/static/js/runtime-main.4814c391.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/renotahoetrails/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/renotahoetrails/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/renotahoetrails/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/renotahoetrails/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/renotahoetrails/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f689706a42e2ceddb3dddab2bab49d76.js"
+  "/renotahoetrails/precache-manifest.5c279046d20fc591ca4b536e271a82d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/renotahoetrails/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
